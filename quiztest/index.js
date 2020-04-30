d4f007d6f5eef344cb427e4c9ae986011095cfc7
tfelic3 <felicien.tesalon@gmail.com>
2020-04-29 23:29:23 -0400

    Add files via upload
    latest js files

--- quiztest/index.js
+++ quiztest/index.js
@@ -12,11 +12,12 @@ let scoreTag = document.querySelector('.score-tag')
 score.style.visibility = "hidden";
 //This creates container for score elements. This will begin as hidden.
 
-
-
 let scoreValue = document.createElement('p');
 scoreValue.classList.add('score');
 scoreTag.insertAdjacentElement('beforeend', scoreValue);
+
+let currentIndex = 0;
+let select;
 
 //Objects quiz questions
 const questionOne = {
@@ -39,15 +40,16 @@ const questionThree ={
 question: "The novel coronavirus appears to have originated in this animal?",
 imgPath: 'images/coronavirusedit.png',
 questionKeys:[{keyOne: "Mouse"},{keyTwo:"Donkey"},{keyThree:"Bat"},{keyFour:"Cow"}],
-correctChoice: "Bat"
+correctChoice: "Bat",
+audioSelection: new Audio('images/kempaudio.m4a')
 }
+
 
 //Function for selecting questions in object
 let questionSelection = () =>{
     
-let select;
-let questionChoices = [questionOne, questionTwo, questionThree];
 
+let questionChoices = [questionOne, questionTwo, questionThree];
 
 function shuffle(){
     let currentPass = questionChoices.length;
@@ -63,6 +65,14 @@ function shuffle(){
 }
 
 shuffle();
+
+currentIndex = questionChoices.indexOf(select);
+nextQuestion = ++currentIndex;
+nextIndex = questionChoices[nextQuestion];
+
+console.log(select)
+console.log(questionChoices[nextQuestion]);
+console.log(questionChoices)
 
 
 let cardOne = document.querySelector('#cardOne');
@@ -84,29 +94,23 @@ cardOne.appendChild(createQuestion);
 
 //Creates button for each question. I want buttons to appear randomly.
 
+
+
 let newButtons;
-let questionButtons = () =>{
-
+select.questionKeys.forEach(question=>{
     
-    select.questionKeys.forEach(question=>{
-        
-        newButtons = document.createElement('button');
-        newButtons.textContent = Object.values(question); 
-        cardOne.insertAdjacentElement('afterend', newButtons);
-    });
-    
-    }
-    questionButtons();
-    
+  newButtons = document.createElement('button');
+  newButtons.id=('questionButtons')
+    newButtons.textContent = Object.values(question); 
+    cardOne.insertAdjacentElement('afterend', newButtons);
+});
 
 
-    let introFunction = ()=>{
-        window.scrollTo({
-          top: 4000,
-          behavior: 'smooth'
-        });
-      }
-introFunction();
+    let answer=document.createElement("p");
+    answer.textContent = "Wrong! The correct choice is " + select.correctChoice;
+    answer.style.color = "red";
+    cardOne.insertAdjacentElement('afterend', answer);
+    answer.style.display = "none";
     
 
     introduction.style.visibility = "hidden";
@@ -114,191 +118,65 @@ introFunction();
     pageIntro.remove();
 
     let buttonHandle = document.querySelectorAll('button');
-
-
-
-    let nextItem = (question,index,array) =>{
-        nextQuestion = (array[index+1]);
-        myImage.src = nextQuestion.imgPath;
-        createQuestion.textContent = nextQuestion.question;
-        let questionButtons = document.querySelectorAll('button');
-        questionButtons.forEach(button=>button.remove());
+ 
 
     
 
-
-
-    
-    }    
-
-    let forwardButton = () =>{
-        let continueButton = document.createElement('button');
-        continueButton.style.backgroundColor ="purple";
-            continueButton.textContent = "Continue"; 
-            document.body.insertAdjacentElement('afterend', continueButton);
-
-        continueButton.addEventListener('click', function(){
-        questionChoices.map(nextItem);
-        
-        
-            
-        
-        
-
-        })
-    }
-
-
-
-
-   
+    let buttonInfo;
     buttonHandle.forEach(function(buttons){
         function buttonInfo(e){
             if(e.target.textContent ===select.correctChoice){
-                buttons.style.backgroundColor ="green";   
+                buttons.style.backgroundColor ="green";  
                 scoreValue.textContent = +1;  
                 if(select.audioSelection){
                     select.audioSelection.play()}  
+                    buttons.disabled = true; 
                   forwardButton();
+                  
             } else {
                 buttons.style.backgroundColor = "red";
-                let answer=document.createElement("p");
-                answer.textContent = "Wrong! The correct choice is " + select.correctChoice;
-                answer.style.color = "red";
-                cardOne.insertAdjacentElement('afterend', answer);
+                if(select.audioSelection){
+                    select.audioSelection.pause()} 
+                    answer.style.display ="block";
+                    buttons.disabled = true; 
             }
         };
         buttons.addEventListener('click', buttonInfo)
+    
+        let resetState = () =>{
+            buttons.removeEventListener('click', buttonInfo)
+            select.audioSelection.pause();   
+            answer.style.display = "none";           
+        }
+
+
+        let forwardButton = () =>{
+            let continueButton = document.createElement('button');
+            continueButton.style.backgroundColor ="purple";
+                continueButton.textContent = "Continue"; 
+                document.body.insertAdjacentElement('afterend', continueButton);
+    
+              let forwardButtonHandle = ()=>{
+                 resetState();
+                select = nextIndex;
+                myImage.src = select.imgPath;
+                createQuestion.textContent = select.question;
+               continueButton.style.visibility = "hidden";               
+              } 
+               
+            continueButton.addEventListener('click',forwardButtonHandle)
+    
+    
+        }
+    
+    
+    
     })
 
 
 };
 
 
+
 introduction.addEventListener('click',questionSelection);
 
-
-
-
-
-/*
-let newArray = () =>{
-    questionChoices.filter(function(){
-        delete select;
-    })  
-}
- newArray();
- console.log(questionChoices);
-};
-
-
-let cardOne = document.querySelector('#cardOne');
-let cardImg = document.querySelector('#image');
-let cardQuestion = document.querySelector('#main-question');
-let button = document.querySelector('#answerChoice');
-
-let myImage = document.createElement('img');
-myImage.id=('image');
-myImage.src = questionOne.imgPath;
-myImage.alt ="Joe Biden";
-
-let createQuestion = document.createElement('h3')
-createQuestion.id= 'question1';
-createQuestion.textContent = questionOne.question;
-
-
-let correctButton1 = document.createElement('button');
-correctButton1.id = "correctChoice";
-correctButton1.textContent = (questionOne.correctChoice);
-
-let otherButton1 = document.createElement('button');
-otherButton1.id = "otherChoice";
-otherButton1.textContent = Object.values(questionOne.questionKeys[0]);
-
-cardOne.appendChild(myImage);
-cardOne.appendChild(createQuestion);
-
-let newAudio = new Audio('images/bidenaudio.m4a')
-
-let questionButtons = () =>{
-questionOne.questionKeys.forEach(question=>{
-    let newButtons = document.createElement('button');
-    newButtons.textContent = Object.values(question); 
-    cardOne.insertAdjacentElement('afterend', newButtons);
-});
-
-}
-questionButtons();
-
-let buttonHandle = document.querySelectorAll('button');
-
-
-buttonHandle.forEach(function(buttons){
-    function buttonInfo(e){
-        if(e.target.textContent ==='Wisconsin'){
-            buttons.style.backgroundColor ="green";   
-            scoreValue.textContent = +1;  
-            newAudio.play();  
-        } else {
-            buttons.style.backgroundColor = "red";
-            let answer=document.createElement("p");
-            answer.textContent = "Wrong! The correct choice is " + Object.values(questionOne.questionKeys[3]);
-            answer.style.color = "red";
-            cardOne.insertAdjacentElement('afterend', answer);
-        }
-    };
-    buttons.addEventListener('click', buttonInfo);
-})
-
-
-
- 
-    /*if (Object.keys(questionOne.questionKeys[i]=== "correctChoice")){
-    item.style.backgroundColor ="green"; 
-    scoreValue.textContent = +1; 
- } else{
-    item.style.backgroundColor = "red"; 
- }
-}
-
- ))
-
- 
-for(let i =0;i<questionOne.questionKeys.length;i++){
-console.log(questionOne.questionKeys[i]);
-
-}
-
-
-buttonHandle.forEach(item=>
-    item.addEventListener('click', function(){
-        if(Object.keys(questionOne.questionKeys==="Wisconsin"){
-            item.style.backgroundColor ="green"; 
-            scoreValue.textContent = +1; 
-        } else{
-            item.style.backgroundColor = "red";
-            let answer=document.createElement("p");
-            answer.textContent = "Wrong! The correct choice is " + questionOne.correctChoice;
-            answer.style.color = "red";
-            cardOne.insertAdjacentElement('afterend', answer);
-        }
-    }))
-
-correctButton1.addEventListener('click', function(){
-    correctButton1.style.backgroundColor= "green";
-    scoreValue.textContent = +1;
-
-} )
-
-
-
-otherButton1.addEventListener('click', function(){
-    otherButton1.style.backgroundColor= "red";
-    correctButton1.style.backgroundColor= "green";
-    let answer=document.createElement("p");
-    answer.textContent = "Wrong! The correct choice is " + questionOne.correctChoice;
-    answer.style.color = "red";
-    cardOne.insertAdjacentElement('afterend', answer);
-})
-
-*/
